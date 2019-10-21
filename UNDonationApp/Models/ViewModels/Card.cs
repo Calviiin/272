3bd2f3e0231a407cb1710f7db4950a2912339975
cassieperkett <u16143923@tuks.co.za>
2019-10-22 01:16:35 +0200

    One ViewModel

---
+++ UNDonationApp/Models/ViewModels/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UNDonationApp.Models;
+
+namespace UNDonationApp.ViewModels
+{
+    public abstract class Card : Payment
+    {
+        public int Card_Number { get; set; }
+        public int CVV { get; set; }
+        public string Card_Holder { get; set; }
+    }
+}
