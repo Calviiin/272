285afa1ab7524f5dc22a2966073cf4c53b23d828
cassieperkett <u16143923@tuks.co.za>
2019-10-22 01:56:14 +0200

    Complete Payment Screen

---
+++ UNDonationApp/Controllers/CompleteController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UNDonationApp.Controllers
+{
+    public class CompleteController : Controller
+    {
+        // GET: Complete
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
